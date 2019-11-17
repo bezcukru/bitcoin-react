4ee72c6910466c6364ae25aba1a09f99f7038aa0
Agata Kuska <kawabezcukru@onet.eu>
2019-11-17 16:40:00 +0100

    Updates

---
+++ precache-manifest.c9c216c93de2ac42b85a981c29f279b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb150f6ad9f38fb204dfd15a838e7f7",
+    "url": "/bitcoin-react/index.html"
+  },
+  {
+    "revision": "1256dcf826317ac06e30",
+    "url": "/bitcoin-react/static/css/main.272955ff.chunk.css"
+  },
+  {
+    "revision": "8a5e8a9911d5c68b8470",
+    "url": "/bitcoin-react/static/js/2.f6039048.chunk.js"
+  },
+  {
+    "revision": "1256dcf826317ac06e30",
+    "url": "/bitcoin-react/static/js/main.9bae3e6b.chunk.js"
+  },
+  {
+    "revision": "153b2880010a67722b78",
+    "url": "/bitcoin-react/static/js/runtime-main.771f2f4f.js"
+  }
+]);
